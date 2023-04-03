2ef868b361fcde34cc04b71c75ab8e378bd49110
Ken Nagamine <nagamine.digitalholding@gmail.com>
2023-04-03 16:04:06 -0700

    "Pushing with .env port"

--- app.js
+++ app.js
@@ -65,6 +65,6 @@ app.get("/post/:extension", function(req, res){
 
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000")
 })
